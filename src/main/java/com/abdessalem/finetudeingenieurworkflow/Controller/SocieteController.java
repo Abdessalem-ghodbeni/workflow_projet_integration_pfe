04412538631345ad8acb0e1bd5cce7d27baa6a8f
abdessalem <slouma4ghodbeny@gmail.com>
2025-02-10 13:12:36 +0100

    filter societe by nom,domaine activité,ville

--- src/main/java/com/abdessalem/finetudeingenieurworkflow/Controller/SocieteController.java
+++ src/main/java/com/abdessalem/finetudeingenieurworkflow/Controller/SocieteController.java
@@ -1,13 +1,76 @@
 package com.abdessalem.finetudeingenieurworkflow.Controller;
 
+import com.abdessalem.finetudeingenieurworkflow.Entites.Etudiant;
+import com.abdessalem.finetudeingenieurworkflow.Entites.Societe;
+import com.abdessalem.finetudeingenieurworkflow.Services.Iservices.ISocieteServices;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/societe")
 @RequiredArgsConstructor
 public class SocieteController {
+
+    private final ISocieteServices societeServices;
+
+
+//    @GetMapping(path = "all")
+//    public ResponseEntity<?> recupererTousLesSocietes(){
+//        List<Societe> listeSocietes= societeServices.GetAllSociete();
+//        return ResponseEntity.ok(listeSocietes);
+//    }
+@GetMapping(path = "all")
+    public ResponseEntity<Page<Societe>> recupererTousLesSocietes(
+
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size) {
+
+    Pageable pageable = PageRequest.of(page, size);
+    Page<Societe> societesPage = societeServices.getAllSocietes(pageable);
+
+    return ResponseEntity.ok(societesPage);
+}
+
+    @GetMapping(path = "/{id}")
+    public  ResponseEntity<?>reupererParId(@PathVariable("id") Long id){
+        Societe societe=societeServices.recupererById(id);
+        return ResponseEntity.ok(societe);
+    }
+
+
+    @GetMapping("search")
+    public ResponseEntity<Page<Societe>> recupererTousLesSocietes(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(required = false) String search) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Societe> societesPage;
+
+        if (search == null || search.isEmpty()) {
+            societesPage = societeServices.getAllSocietes(pageable);
+        } else {
+            societesPage = societeServices.searchSocietes(search, pageable);
+        }
+
+        return ResponseEntity.ok(societesPage);
+    }
+
+
+
+    @GetMapping("/filtered")
+    public Page<Societe> getFilteredSocietes(
+            @RequestParam(required = false) List<String> secteurs,
+            @RequestParam(required = false) List<String> villes,
+            @RequestParam(required = false) Boolean active,
+            Pageable pageable) {
+        return societeServices.getFilteredSocietes(secteurs, villes, active, pageable);
+    }
 }
--- src/main/java/com/abdessalem/finetudeingenieurworkflow/Services/ServiceImplementation/ISocieteServicesImpl.java
+++ src/main/java/com/abdessalem/finetudeingenieurworkflow/Services/ServiceImplementation/ISocieteServicesImpl.java
@@ -1,9 +1,13 @@
 package com.abdessalem.finetudeingenieurworkflow.Services.ServiceImplementation;
 
 import com.abdessalem.finetudeingenieurworkflow.Entites.Societe;
+import com.abdessalem.finetudeingenieurworkflow.Exception.RessourceNotFound;
+import com.abdessalem.finetudeingenieurworkflow.Repository.ISocieteRepository;
 import com.abdessalem.finetudeingenieurworkflow.Services.Iservices.ISocieteServices;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,13 +16,32 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class ISocieteServicesImpl implements ISocieteServices {
+    private final ISocieteRepository societeRepository;
     @Override
     public List<Societe> GetAllSociete() {
-        return null;
+        List<Societe> listeSociete=societeRepository.findAll();
+        return listeSociete;
     }
 
     @Override
     public Societe recupererById(Long idSociete) {
-        return null;
+        return societeRepository.findById(idSociete).orElseThrow(() -> new RessourceNotFound("societe avec l'ID " + idSociete + " n'existe pas."));
     }
+
+    @Override
+    public Page<Societe> getAllSocietes(Pageable pageable) {
+        return societeRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Societe> searchSocietes(String keyword, Pageable pageable) {
+        return societeRepository.findByNomContainingIgnoreCaseOrSecteurActiviteContainingIgnoreCase(keyword, keyword, pageable);
+    }
+
+    @Override
+    public Page<Societe> getFilteredSocietes(List<String> secteurs, List<String> villes, Boolean active, Pageable pageable) {
+        return societeRepository.findFilteredSocietes(secteurs, villes, active, pageable);
+    }
+
+
 }
