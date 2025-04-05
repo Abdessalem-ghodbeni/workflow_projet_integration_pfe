21f9969db03ddcad7369345d71e3f5e328140c24
abdessalem <slouma4ghodbeny@gmail.com>
2025-04-05 22:20:00 +0100

    xpose api

---
+++ src/main/java/com/abdessalem/finetudeingenieurworkflow/Controller/EpicController.java
@@ -0,0 +1,45 @@
+package com.abdessalem.finetudeingenieurworkflow.Controller;
+
+import com.abdessalem.finetudeingenieurworkflow.Entites.ApiResponse;
+import com.abdessalem.finetudeingenieurworkflow.Entites.Epic;
+import com.abdessalem.finetudeingenieurworkflow.Services.Iservices.IEpicServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/epic")
+@RequiredArgsConstructor
+public class EpicController {
+    private final IEpicServices epicServices;
+//    @PostMapping("/{projetId}/{etudiantId}")
+//    public ResponseEntity<ApiResponse> addEpic(@PathVariable Long projetId,
+//                                               @PathVariable Long etudiantId,
+//                                               @RequestBody Epic epic) {
+//        ApiResponse response = epicService.addEpicToProject(projetId, etudiantId, epic);
+//        HttpStatus status = response.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
+//        return new ResponseEntity<>(response, status);
+//    }
+
+
+    @PostMapping("/{projetId}/{etudiantId}")
+    public ResponseEntity<ApiResponse> addEpic(@PathVariable Long projetId,
+                                               @PathVariable Long etudiantId,
+                                               @RequestBody Epic epic) {
+
+        try {
+            ApiResponse response = epicServices.addEpicToProject(projetId, etudiantId, epic);
+
+            if (!response.isSuccess()) {
+                return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+
+                return new ResponseEntity<>(response, HttpStatus.CREATED);
+            }
+        } catch (Exception exception) {
+            return new ResponseEntity<>(new ApiResponse(exception.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
