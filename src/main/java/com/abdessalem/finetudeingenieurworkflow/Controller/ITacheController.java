b9927e207115d7231c4ba5f25bab19fcd4161d1e
abdessalem <slouma4ghodbeny@gmail.com>
2025-04-07 18:09:06 +0100

    exposer api

---
+++ src/main/java/com/abdessalem/finetudeingenieurworkflow/Controller/ITacheController.java
@@ -0,0 +1,39 @@
+package com.abdessalem.finetudeingenieurworkflow.Controller;
+
+import com.abdessalem.finetudeingenieurworkflow.Entites.ApiResponse;
+import com.abdessalem.finetudeingenieurworkflow.Entites.Epic;
+import com.abdessalem.finetudeingenieurworkflow.Entites.TacheRequest;
+import com.abdessalem.finetudeingenieurworkflow.Services.Iservices.ITacheServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/tache")
+@RequiredArgsConstructor
+public class ITacheController {
+    private final ITacheServices tacheServices;
+
+    @PostMapping("/ajouter/{etudiantId}")
+    public ResponseEntity<ApiResponse> ajouterTache(
+                                               @PathVariable("etudiantId") Long etudiantId,
+                                               @RequestBody TacheRequest tacheRequest) {
+
+        try {
+            ApiResponse response = tacheServices.ajouterTache(tacheRequest,etudiantId);
+
+            if (!response.isSuccess()) {
+                return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            } else {
+
+                return new ResponseEntity<>(response, HttpStatus.CREATED);
+            }
+        } catch (Exception exception) {
+            return new ResponseEntity<>(new ApiResponse(exception.getMessage(), false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+}
